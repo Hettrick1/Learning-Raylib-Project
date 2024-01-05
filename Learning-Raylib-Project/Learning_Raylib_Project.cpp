dbbde466cbf50e4f3ea2a07c8375ef4d00767511
Hettrick1 <evann.jacquot@gmail.com>
2024-01-05 11:01:39 +0100

    [wip] continue learning cpp

--- Learning-Raylib-Project/Learning_Raylib_Project.cpp
+++ Learning-Raylib-Project/Learning_Raylib_Project.cpp
@@ -1,6 +1,16 @@
 #include <raylib.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
 int main()
 {
+    vector<string> noms {"Annie", "Evann", "Jean", "Michel"};
+
+    for (string nom : noms)
+    {
+        cout << "Bonjour " + nom << endl;
+    }
     return 0;
 }
