162b1d9d913cc68bc4b99e6ff9cfea7ed461d02d
Hettrick1 <evann.jacquot@gmail.com>
2024-01-21 21:59:33 +0100

    [done] Create a game
    You have to guess which number between 1 and 100 has been picked up by the machine.

--- Learning-Raylib-Project/Learning_Raylib_Project.cpp
+++ Learning-Raylib-Project/Learning_Raylib_Project.cpp
@@ -1,16 +1,42 @@
-#include <raylib.h>
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 int main()
 {
-    vector<string> noms {"Annie", "Evann", "Jean", "Michel"};
-
-    for (string nom : noms)
+    srand(time(nullptr));
+    int nombreChoisi = rand() % 100 + 1;
+    int proposition = 0;
+    int nombreTentatives = 0;
+    
+    do
     {
-        cout << "Bonjour " + nom << endl;
-    }
+        cout << "-------------------\n" << "Quel est le bon nombre ? \nEntrez un nombre entre 1 et 100\n";
+        cin >> proposition;
+    
+        if (proposition < nombreChoisi && proposition >= 1)
+        {
+            cout << "-------------------\n" << "Le nombre choisi est trop petit \n";
+        }
+
+        else if (proposition > nombreChoisi && proposition <= 100)
+        {
+            cout << "-------------------\n" << "Le nombre choisi est trop grand \n";
+        }
+        
+        else if (proposition != nombreChoisi)
+        {
+            cout << "-------------------\n" << "Le nombre entre n'est pas valide";
+        }
+
+        nombreTentatives += 1;
+        
+    } while (proposition != nombreChoisi);
+
+    cout << "Vous avez gagne ! \n" << "Cela a necessite : " << nombreTentatives << " tentatives.";
+    
     return 0;
 }
