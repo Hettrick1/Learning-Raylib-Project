c6b164e3497f57c1fcc157c9d567f5e3de3be896
Hettrick1 <evann.jacquot@gmail.com>
2024-01-23 21:32:10 +0100

    [bug] ca pose plusieur fois la question et ca répond sans attendre la réponse

--- Learning-Raylib-Project/Learning_Raylib_Project.cpp
+++ Learning-Raylib-Project/Learning_Raylib_Project.cpp
@@ -10,12 +10,22 @@ int main()
     string question;
     do
     {
-        cout << "Pose moi une question";
+        question = "";
+        srand(time(nullptr));
+        int nombreChoisi = rand() % 100 + 1;
+        cout << "Pose moi une question\n";
         cin >> question;
-        
+        if (nombreChoisi <= 50)
+        {
+            cout << "Oui.\n";
+        }
+        else
+        {
+            cout << "Non.\n";
+        }
     }while (question != "partir");
 
-    cout << "Adieu.";
+    cout << "Adieu.\n";
     
     return 0;
 }
